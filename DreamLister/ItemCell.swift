40feedf267987f359a43a009d57d521a6c521ecd
Kevin Peng <aoenth@gmail.com>
2017-05-24 15:59:06 -0400

    Hook up IBOutlets, UITableViewDelegate, UITableViewDataSource

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Kevin on 2017-05-24.
+//  Copyright © 2017 Monorail Apps. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var details: UILabel!
+    
+    
+    
+}
