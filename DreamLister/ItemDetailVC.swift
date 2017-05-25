71633b5fffc087d238509090c3f0c185e06936e8
Kevin Peng <aoenth@gmail.com>
2017-05-25 12:01:54 -0400

    119 - Textfields, picker view UI

---
+++ DreamLister/ItemDetailVC.swift
@@ -0,0 +1,40 @@
+//
+//  ItemDetailVC.swift
+//  DreamLister
+//
+//  Created by Kevin on 2017-05-25.
+//  Copyright © 2017 Monorail Apps. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailVC: UIViewController {
+
+    @IBOutlet weak var storePicker: UIPickerView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
